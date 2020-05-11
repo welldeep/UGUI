c5354763a09814a6e6bba57f7b9d53af6b3f6e8d
welldeep <welldeep16@gmail.com>
2020-05-11 16:22:14 +0800

    add comment

--- ugui.c
+++ ugui.c
@@ -5265,14 +5265,14 @@ void _UG_PutChar( char chr, UG_S16 x, UG_S16 y, UG_COLOR fc, UG_COLOR bc, const 
 
    switch ( bt )
    {
-      case 0xF6: bt = 0x94; break; // ö
-      case 0xD6: bt = 0x99; break; // Ö
-      case 0xFC: bt = 0x81; break; // ü
-      case 0xDC: bt = 0x9A; break; // Ü
-      case 0xE4: bt = 0x84; break; // ä
-      case 0xC4: bt = 0x8E; break; // Ä
-      case 0xB5: bt = 0xE6; break; // µ
-      case 0xB0: bt = 0xF8; break; // °
+      case 0xF6: bt = 0x94; break; // ï¿½
+      case 0xD6: bt = 0x99; break; // ï¿½
+      case 0xFC: bt = 0x81; break; // ï¿½
+      case 0xDC: bt = 0x9A; break; // ï¿½
+      case 0xE4: bt = 0x84; break; // ï¿½
+      case 0xC4: bt = 0x8E; break; // ï¿½
+      case 0xB5: bt = 0xE6; break; // ï¿½
+      case 0xB0: bt = 0xF8; break; // ï¿½
    }
 
    if (bt < font->start_char || bt > font->end_char) return;
@@ -6479,6 +6479,12 @@ UG_S16 UG_WindowGetOuterHeight( UG_WINDOW* wnd )
    return h;
 }
 
+/**
+ * @brief 
+ * 
+ * @param wnd 
+ * @return UG_RESULT 
+ */
 UG_RESULT _UG_WindowDrawTitle( UG_WINDOW* wnd )
 {
    UG_TEXT txt;
